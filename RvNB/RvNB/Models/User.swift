bfe19b6b4ee86bafd2f15a442eb8724541841393
Alex Shillingford <alex.shillingford@icloud.com>
2019-10-21 14:18:48 -0600

    Create UserRepresentation struct; create CodingKeys enum within UserRepresentation struct to properly decode the properties given/posted to and from the backend for user object.

---
+++ RvNB/RvNB/Models/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  RvNB
+//
+//  Created by Alex Shillingford on 10/21/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import Foundation
+
+struct UserRepresentation {
+    enum CodingKeys: String, CodingKey {
+        case firstName = "first_name"
+        case lastName = "last_name"
+    }
+    
+    var id: Int
+    var firstName: String
+    var lastName: String
+    var email: String
+    var username: String
+    var password: String
+    var owner: Bool
+    var avatar: URL
+}
