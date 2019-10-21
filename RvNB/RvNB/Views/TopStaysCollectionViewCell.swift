46854f43a29e25a92752c4ab4d73d3c1a972159a
Alex Shillingford <alex.shillingford@icloud.com>
2019-10-21 15:45:31 -0600

    Add in MainTabViewController.swift, TopStaysCollectionViewController.swift, and TopStaysCollectionViewCell.swift from Dani's branch to resolve merge conflicts

---
+++ RvNB/RvNB/Views/TopStaysCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TopStaysCollectionViewCell.swift
+//  RvNB
+//
+//  Created by Lambda_School_Loaner_167 on 10/21/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class TopStaysCollectionViewCell: UICollectionViewCell {
+    
+}
