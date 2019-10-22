4c0fd3c5d560dea990f4e91f12dda001d12b6797
Alex Shillingford <alex.shillingford@icloud.com>
2019-10-21 18:30:08 -0600

    Create UserController.swift file; add NetworkError enum and HTTPMethod enum; add `createUser()` function for mock data

---
+++ RvNB/RvNB/Model Controllers/UserController.swift
@@ -0,0 +1,37 @@
+//
+//  UserController.swift
+//  RvNB
+//
+//  Created by Alex Shillingford on 10/21/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+import Foundation
+
+enum NetworkError: Error {
+    case noAuth
+    case badAuth
+    case otherError
+    case badData
+    case noDecode
+}
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
+class UserController {
+    let baseURL = URL(string: "https://rvnb-mock-database.firebaseio.com/")!
+    
+    let usersArray: [UserRepresentation] = []
+    
+    func createUser(firstName: String?, lastName: String?, email: String?, username: String, password: String, owner: Bool, avatar: URL) -> UserRepresentation {
+        let id: Int = usersArray.count + 1
+        
+        let newUserRepresentation = UserRepresentation(id: id, firstName: firstName, lastName: lastName, email: email, username: username, password: password, owner: owner, avatar: avatar)
+        
+        return newUserRepresentation
+    }
+}
