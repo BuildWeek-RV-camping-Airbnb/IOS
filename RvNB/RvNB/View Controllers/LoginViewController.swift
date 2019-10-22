3704c89da775813c1e9524f7258783b770b90770
Dani Blackwell <bleuxgenius@gmail.com>
2019-10-22 18:07:28 -0500

    login screen UI complete. view controller completed.

---
+++ RvNB/RvNB/View Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  RvNB
+//
+//  Created by Lambda_School_Loaner_167 on 10/22/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
