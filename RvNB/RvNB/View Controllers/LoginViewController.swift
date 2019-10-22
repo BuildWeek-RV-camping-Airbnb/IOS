21a4b05b9015c1aaa1a86f79993d16fda1b0b651
Dani Blackwell <bleuxgenius@gmail.com>
2019-10-22 18:08:11 -0500

    Created Login storyboard

--- RvNB/RvNB/View Controllers/LoginViewController.swift
+++ RvNB/RvNB/View Controllers/LoginViewController.swift
@@ -9,11 +9,43 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+//    MARK: Outlets
+    @IBOutlet weak var topConstraintHeight: NSLayoutConstraint!
+    
+    @IBOutlet weak var logoTopConstraint: NSLayoutConstraint!
+    
+//    MARK: ACTIONS
+    @IBAction func hideSigninPop(_ sender: UIButton) {
+        topConstraintHeight.constant = 800;
+        logoTopConstraint.constant = 187;
+        
+        UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0, options: .curveEaseOut, animations: { self.view.layoutIfNeeded() }, completion: nil)
+        print("clicked")
+        
+    }
+    
+    
+    @IBAction func ShowSignInPop(_ sender: Any) {
+        
+    topConstraintHeight.constant = 0;
+        logoTopConstraint.constant = 100;
+        
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0, options: .curveEaseOut, animations: { self.view.layoutIfNeeded() }, completion: nil)
+        print("clicked")
+    }
+    
+    
+    
+    // MARK: View Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        topConstraintHeight.constant = 800;
+        logoTopConstraint.constant = 187;
     }
     
 
