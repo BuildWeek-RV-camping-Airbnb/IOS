45d60252886d21ad3dcb2abe2234d17331d23034
Alex Shillingford <alex.shillingford@icloud.com>
2019-10-23 11:53:54 -0600

    Add signUpLogIn() function for authentication. Should be able to receive token and user id

--- RvNB/RvNB/Model Controllers/APIController.swift
+++ RvNB/RvNB/Model Controllers/APIController.swift
@@ -40,6 +40,62 @@ class UserController {
         postNewUser(userRep: newUserRepresentation)
         return newUserRepresentation
     }
+    
+    
+    // MARK: - Networking-SignIn
+    func signUpLogIn(firstName: String?, lastName: String?, email: String?, username: String, password: String, owner: Bool, avatar: URL?, completion: @escaping (NetworkError?) -> Void = { _ in }) {
+        let requestURL = baseURL
+            .appendingPathComponent("api")
+            .appendingPathComponent("users")
+            .appendingPathComponent("login")
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        var newUserRep = createUser(firstName: firstName, lastName: lastName, email: email, username: username, password: password, owner: owner, avatar: nil)
+        
+        let userParameters: [String: String] = ["username" : username, "password" : password]
+        
+        do {
+            request.httpBody = try JSONEncoder().encode(userParameters)
+        } catch {
+            NSLog("Error signing in on line \(#line) in \(#file): \(error)")
+            completion(.noAuth)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                NSLog("\(NSError(domain: "", code: response.statusCode, userInfo: nil))")
+                completion(.postRequestError)
+                return
+            }
+            
+            if let error = error {
+                NSLog("Error on line \(#line) in \(#file): \(error)")
+                completion(.otherError)
+                return
+            }
+            
+            guard let data = data else {
+                completion(.badData)
+                return
+            }
+            
+            do {
+                self.bearer = try JSONDecoder().decode(Bearer.self, from: data)
+                newUserRep.id = try JSONDecoder().decode(Int.self, from: data)
+            } catch {
+                NSLog("Error decoding token or user ID on line \(#line) in \(#file): \(error)")
+                completion(.otherError)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
+    }
+    
     // MARK: - Networking-GET User
     func fetchUserIDFromServer(completion: @escaping (NetworkError?) -> Void = { _ in }) {
         let requestURL = baseURL.appendingPathComponent("api").appendingPathComponent("users")
