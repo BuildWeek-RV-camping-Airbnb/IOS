edc846a6dcc72ba889c63027ac929fd78da93fd4
Alex Shillingford <alex.shillingford@icloud.com>
2019-10-23 11:52:55 -0600

    Add optional `bearer` property to UserController; change avatar parameter in createUser() method to optional (URL?)

--- RvNB/RvNB/Model Controllers/APIController.swift
+++ RvNB/RvNB/Model Controllers/APIController.swift
@@ -30,10 +30,10 @@ enum HTTPMethod: String {
 
 class UserController {
     let baseURL = URL(string: "https://bw-rvnb.herokuapp.com")!
-    
+    var bearer: Bearer?
     var usersArray: [UserRepresentation] = []
     
-    func createUser(firstName: String?, lastName: String?, email: String?, username: String, password: String, owner: Bool, avatar: URL) -> UserRepresentation {
+    func createUser(firstName: String?, lastName: String?, email: String?, username: String, password: String, owner: Bool, avatar: URL?) -> UserRepresentation {
         
         let newUserRepresentation = UserRepresentation(firstName: firstName, lastName: lastName, email: email, username: username, password: password, owner: owner, avatar: avatar)
         
