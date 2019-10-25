f5503d82600684545c284c94afc86ef65ec12ab7
Alex Shillingford <alex.shillingford@icloud.com>
2019-10-24 21:58:01 -0400

    Add Models for Property, Listing, Amenity, and Reservation

---
+++ RvNB/RvNB/Models/Amenity.swift
@@ -0,0 +1,15 @@
+//
+//  Amenity.swift
+//  RvNB
+//
+//  Created by Alex Shillingford on 10/24/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import Foundation
+
+struct AmenityRepresentation: Codable {
+    var name: String
+    var icon: String
+    var id: Int?
+}
